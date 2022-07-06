5b2141f2fd6e2fb457cb86e5412db2b3e6f68ee6
Marvin Abante <msmarvin2022@gmail.com>
2022-07-06 17:07:58 +0800

    Added header option handling to users.php

--- v0/controller/users.php
+++ v0/controller/users.php
@@ -17,7 +17,7 @@ try {
     exit;
 }
 
-// handle options request method for CORS
+//Handle options request method for CORS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Methods: POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
